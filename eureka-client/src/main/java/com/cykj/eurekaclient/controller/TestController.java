ffabb97f2d4c8e761d0c1a7c9f2f1f76ea6fc562
haiyang5233233 <122022909@qq.com>
2020-09-03 16:33:32 +0800

    提交修改第二次 zhaohy

--- eureka-client/src/main/java/com/cykj/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/cykj/eurekaclient/controller/TestController.java
@@ -25,7 +25,7 @@ public class TestController {
     public Object getUser() {
         System.out.println("请求开始");
         User user = new User();
-        User user2 = new User();
+        User user2 = new User();//按时发达撒旦法撒旦法
         //return this.restTemplate.getForObject("http://server-provider/user/reg", User.class);
         return userFeign.getUser();
     }
